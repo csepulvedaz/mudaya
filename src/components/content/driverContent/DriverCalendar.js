2b8b81c377c77a8d2c2c01a9fbafebdbc0e097c5
mcsrk <jfacostamu@unal.edu.co>
2020-05-01 22:17:51 -0500

    :lipstick: Calendar style: amber. Highligth Today Column

--- src/components/content/driverContent/DriverCalendar.js
+++ src/components/content/driverContent/DriverCalendar.js
@@ -1,7 +1,12 @@
 import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
+import { withStyles } from '@material-ui/core/styles';
+import { fade } from '@material-ui/core/styles/colorManipulator';
 import { ViewState } from "@devexpress/dx-react-scheduler";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import { amber } from "@material-ui/core/colors";
+
 import {
     Scheduler,
     WeekView,
@@ -10,22 +15,49 @@ import {
     DateNavigator,
     TodayButton,
 } from "@devexpress/dx-react-scheduler-material-ui";
-import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
-import { blue } from "@material-ui/core/colors";
+
 
 const useStyles = makeStyles((theme) => ({
     content: {},
+    todayCell: {
+        backgroundColor: fade("#FCB625", 0.1),
+        '&:hover': {
+          backgroundColor: fade("#FCB625", 0.14),
+        },
+        '&:focus': {
+          backgroundColor: fade("#FCB625", 0.16),
+        },
+      },
+      today: {
+        backgroundColor: fade("#FCB625", 0.16),
+      },
 }));
 
 const DriverCalendar = (props) => {
     const classes = useStyles();
-    const theme = createMuiTheme({ palette: { type: "light", primary: blue } });
+    const theme = createMuiTheme({ palette: { type: "light", primary: amber } });
 
     const today = new Date();
     const todayDate = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
     const manualTraduction = {today:"Hoy"};
 
     const [currentDate, setCurrentDate] = useState(todayDate);
+
+    const TimeTableCell = props => {
+        const { startDate } = props;
+        const date = new Date(startDate);
+        if (date.getDate() === new Date().getDate()) {
+          return <WeekView.TimeTableCell {...props} className={classes.todayCell} />;
+        } return <WeekView.TimeTableCell {...props} />;
+    };
+
+    const DayScaleCell = props => {
+        const { startDate, today } = props;
+        if (today) {
+          return <WeekView.DayScaleCell {...props} className={classes.today} />;
+        }  return <WeekView.DayScaleCell {...props} />;
+    };
+    //const DayScaleCell = withStyles(style, { name: 'DayScaleCell' })(DayScaleCellBase);
 
     return (
         <MuiThemeProvider theme={theme}>
@@ -43,6 +75,8 @@ const DriverCalendar = (props) => {
                         startDayHour={6}
                         endDayHour={18}
                         cellDuration={60}
+                        dayScaleCellComponent={DayScaleCell}
+                        timeTableCellComponent={TimeTableCell}
                     ></WeekView>
                     <Toolbar />
                     <DateNavigator />
