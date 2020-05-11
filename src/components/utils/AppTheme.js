5a4b54dbd68a88c0fea3fb15aaebf0240da952ab
mcsrk <jfacostamu@unal.edu.co>
2020-05-11 14:20:18 -0500

    :lipstick: applied theme to login

--- src/components/utils/AppTheme.js
+++ src/components/utils/AppTheme.js
@@ -1,7 +1,12 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 
+//Paleta cesar
+const a = "#4e9fbf";
 const blueOne = "#63b6bf";
+const c = "#8fd1d9";
+const d = "#f2cf8d";
 const yellowOne = "#f2bc79";
+
 
 const theme = createMuiTheme({
   palette: {
@@ -12,5 +17,7 @@ const theme = createMuiTheme({
         main: yellowOne,
     },
   },
-});
+}); 
+theme.shadows[25] ="0px 3px 3px -2px #8FD1D9,0px 3px 4px 0px #8FD1D9,0px 1px 8px 0px #8FD1D9";
+
 export default theme;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -20,6 +20,7 @@ import * as Yup from "yup";
 import AuthContext from "../context/auth-context";
 import { LOGIN } from "../graphql/queries";
 import logo from "../assets/logo.png"
+import theme from "../components/utils/AppTheme";
 
 const useStyles = makeStyles((theme) => ({
     "@global": {
@@ -38,7 +39,7 @@ const useStyles = makeStyles((theme) => ({
         alignItems: "center",
         backgroundColor: "#fff",
         padding: "30px",
-        boxShadow: "1px 1px 5px #8FD1D9",
+        boxShadow: theme.shadows[25] ,
         borderRadius: "5px",
     },
     form: {
@@ -52,12 +53,16 @@ const useStyles = makeStyles((theme) => ({
     submit: {
         margin: theme.spacing(4, 0, 3),
         width: "100%",
-        background: "#4e9fbf",
+        background: theme.palette.primary.main,
         borderRadius: 9,
         border: 0,
         color: "white",
         height: 48,
-        boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+        boxShadow: theme.shadows[2],
+        '&:hover': {
+            background: theme.palette.primary.light,
+            boxShadow: theme.shadows[4],
+        }
     },
     truck: {
         fontSize: "100px",
@@ -69,11 +74,11 @@ const useStyles = makeStyles((theme) => ({
         top: "50%",
         left: "40%",
     },
-    errorMessage: { marginTop: "15px", color: "red", fontSize: "12px" },
+    errorMessage: { marginTop: "15px", color: theme.palette.error.main, fontSize: "12px" },
     notchedOutline: {},
     focused: {
         "&$focused $notchedOutline": {
-            border: "1px #000 solid !important",
+            border: `1px ${theme.palette.primary.light} solid !important`,
         },
     },
     logo:{
@@ -242,6 +247,7 @@ const Login = () => {
                                             to="/registro"
                                             style={{
                                                 textDecoration: "none",
+                                                color:theme.palette.primary.main
                                             }}
                                         >
                                             Entra, es gratis!
