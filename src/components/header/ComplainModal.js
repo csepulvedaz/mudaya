ba08edb8e87fe87427cee52f11ab83818575a8ec
Camilo Sepulveda <csepulvedaz@unal.edu.co>
2020-06-07 11:26:42 -0500

    :zap: Quejas

--- src/components/header/ComplainModal.js
+++ src/components/header/ComplainModal.js
@@ -1,11 +1,9 @@
-import React, { useContext } from "react";
+import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import AuthContext from "../../context/auth-context";
-import { Modal, Layout, Row, Button,Col } from "antd";
+import { Modal, Layout, Row, Button, Col } from "antd";
 import * as Yup from "yup";
-import { ErrorMessage, Form, Formik } from "formik";
+import { Form, Formik } from "formik";
 import { TextField } from "@material-ui/core";
-
 
 const useStyles = makeStyles((theme) => ({
     paper: {
@@ -19,33 +17,18 @@ const useStyles = makeStyles((theme) => ({
     form: {
         width: "100%", // Fix IE 11 issue.
     },
-    rows: {
-        marginTop: theme.spacing(1),
-    },
     buttons: {
         marginTop: theme.spacing(2),
         width: "100%",
         display: "flex",
         flexDirection: "row",
-        justifyContent: "center",
+        justifyContent: "flex-start",
         alignItems: "center",
         backgroundColor: "#fff",
-    },
-    backButton: {
-        width: "124px",
-        height: "35px",
-        marginTop: theme.spacing(3),
-        margin: "5px 0px",
-        borderRadius: "7px",
-        background: "#fff",
-        color: theme.palette.primary.main,
-        fontWeight: "600",
-        boxShadow: "0 3px 3px rgba(0, 0, 0, 0.16)",
     },
     button: {
         width: "124px",
         height: "35px",
-        marginTop: theme.spacing(3),
         margin: "5px 0px",
         borderRadius: "7px",
         background: theme.palette.primary.main,
@@ -56,8 +39,7 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-const ComplainModal = (props) =>{
-    const context = useContext(AuthContext);
+const ComplainModal = (props) => {
     const classes = useStyles();
 
     const handleCancel = () => {
@@ -65,24 +47,24 @@ const ComplainModal = (props) =>{
         props.setVisible(false);
     };
 
-    return(
+    return (
         <Modal
             visible={props.visible}
             centered
-            title="inserte su queja"
+            title="Queja"
             onCancel={handleCancel}
-            footer={[]}
+            footer={null}
         >
             <Layout className={classes.paper}>
                 <Formik
                     initialValues={{
-                        complain:"",
+                        complain: "",
                     }}
                     validationSchema={Yup.object({
                         complain: Yup.string().required("Campo requerido!"),
                     })}
-                    onSubmit={(values)=>{
-                        console.log("nice queja")
+                    onSubmit={(values) => {
+                        console.log("nice queja");
                     }}
                 >
                     {(formik) => (
@@ -92,40 +74,21 @@ const ComplainModal = (props) =>{
                             onSubmit={formik.handleSubmit}
                             noValidate
                         >
-                            <Row className={classes.rows}>
+                            <Row>
                                 <TextField
                                     fullWidth
                                     variant="outlined"
                                     margin="dense"
-                                    label="queja" 
+                                    placeholder="Por favor ingrese su queja dentro de este cuadro, no tardaremos en reponder :)"
                                     multiline
-                                    rows="4"  
+                                    error={formik.errors.complain}
+                                    rows="5"
                                     name="complain"
                                     type="text"
                                     {...formik.getFieldProps("complain")}
                                 />
-                                <ErrorMessage name="complain">
-                                    {(msg) => (
-                                        <p
-                                            className={
-                                                classes.helperText
-                                            }
-                                        >
-                                            {msg}
-                                        </p>
-                                    )}
-                                </ErrorMessage>
                             </Row>
-                            <Row gutter={16} className={classes.buttons}>
-                                <Col>
-                                    <Button
-                                        variant="contained"
-                                        onClick={handleCancel}
-                                        className={classes.backButton}
-                                    >
-                                        volver
-                                    </Button>
-                                </Col>
+                            <Row className={classes.buttons}>
                                 <Col>
                                     <Button
                                         type="submit"
@@ -133,16 +96,14 @@ const ComplainModal = (props) =>{
                                         onClick={formik.handleSubmit}
                                         className={classes.button}
                                     >
-                                        enviar
-                                    </Button>     
+                                        Enviar
+                                    </Button>
                                 </Col>
                             </Row>
                         </Form>
                     )}
-
                 </Formik>
             </Layout>
-
         </Modal>
     );
 };
